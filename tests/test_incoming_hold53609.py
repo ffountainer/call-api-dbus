fda0ea09c4221f4d7894b05b6638e30d3ae84d3e
Veronika Levasheva <v.levasheva@innopolis.university>
2026-04-24 09:00:11 +0300

    feat: finish implementation of test 53609

--- tests/test_incoming_hold53609.py
+++ tests/test_incoming_hold53609.py
@@ -6,7 +6,7 @@ import re
 import time
 
 
-class TestIncomingCall50557:
+class TestIncomingHold53609:
     def setup_method(self):
         self.driver = driver_init.create_driver()
         # create an appium driver for the tests :0
@@ -15,13 +15,9 @@ class TestIncomingCall50557:
 
         driver = self.driver
 
-        # step 1 (click on the icon)
+        # step 1 (activate app)
 
         driver.activate_app("ru/auroraos/demos/CallApiDBus")
-
-        # I do not have access to the system elements,
-        # but I could have used this If I did
-        # driver.find_element(By.ID, "CallApiDBus").click()
 
         # 1.1 the main page is displayed
 
@@ -82,13 +78,27 @@ class TestIncomingCall50557:
 
         # 1.4
 
-        driver.screenshot("./../screenshots/actual/50557/main_no_options.png")
+        driver.screenshot("./../screenshots/actual/53609/main_no_options.png")
+
+        # step 2
+
+        hold_switch.click()
+
+        # 2.1
+
+        assert hold_switch.is_selected()
+
+        # 2.2
+
+        driver.screenshot("./../screenshots/actual/53609/hold_on.png")
+
+        # step 3
 
         # step 2 (click on incoming button)
 
         incoming.click()
 
-        # 2.1
+        # 3.1
 
         def get_timer_value():
             text = driver.find_element(By.ID, "incomingCallButton").text
@@ -106,15 +116,15 @@ class TestIncomingCall50557:
         # can we see 0?
         WebDriverWait(driver, 7).until(lambda d: get_timer_value() in (0, None))
 
-        # 2.2 and 2.3
+        # 3.2 and 3.3
 
         # checked in previous step while checking timer
 
-        # 2.4
+        # 3.4
 
-        driver.screenshot("./../screenshots/actual/50557/timer.png")
+        driver.screenshot("./../screenshots/actual/53609/timer.png")
 
-        # 2.5
+        # 3.5
 
         contact = WebDriverWait(driver, 3).until(
             EC.presence_of_element_located(
@@ -124,27 +134,27 @@ class TestIncomingCall50557:
 
         assert contact.is_displayed()
 
-        # 2.6 I cannot check vibration state here
+        # 3.6 I cannot check vibration state here
 
-        # 2.7
+        # 3.7
 
-        driver.screenshot("./../screenshots/actual/50557/start_incoming.png")
+        driver.screenshot("./../screenshots/actual/53609/start_incoming.png")
 
-        # step 3
+        # step 4
 
         green = driver.find_element(By.XPATH, "//*[contains(@text, 'Принять')]")
 
         green.click()
 
-        # 3.1
+        # 4.1
 
         driver.execute_script("app:waitForPageChange", 3000)
 
-        # 3.2 cannot check vibro
+        # 4.2 cannot check vibro
 
-        # 3.3 cannot check music
+        # 4.3 cannot check music
 
-        # 3.4
+        # 4.4
 
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
@@ -152,48 +162,93 @@ class TestIncomingCall50557:
             )
         )
 
-        # 3.5
+        # 4.5
 
         speaker = driver.find_element(By.XPATH, "//*[contains(@text, 'Динамик')]")
         volume_off = driver.find_element(By.XPATH, "//*[contains(@text, 'Откл. звук')]")
         dbus_api = driver.find_element(
             By.XPATH, "//*[contains(@text, 'Call API DBus')]"
         )
+        hold = driver.find_element(By.XPATH, "//*[contains(@text, 'Удержание')]")
 
         assert speaker.is_displayed() and speaker.is_enabled()
         assert volume_off.is_displayed() and volume_off.is_enabled()
         assert dbus_api.is_displayed() and dbus_api.is_enabled()
+        assert hold.is_displayed() and hold.is_enabled()
 
-        # 3.6
+        # 4.6
 
         keyboard = driver.find_element(By.XPATH, "//*[contains(@text, 'Клавиатура')]")
         record = driver.find_element(By.XPATH, "//*[contains(@text, 'Запись')]")
-        hold = driver.find_element(By.XPATH, "//*[contains(@text, 'Удержание')]")
 
         assert keyboard.is_displayed() and keyboard.is_enabled()
         assert record.is_displayed() and record.is_enabled()
-        assert hold.is_displayed() and hold.is_enabled()
 
-        # 3.7
+        # 4.7
 
-        driver.screenshot("./../screenshots/actual/50557/call_in_process.png")
+        driver.screenshot("./../screenshots/actual/53609/call_in_process.png")
 
-        # step 4
+        # step 5
+
+        assert not hold.is_selected()
+
+        hold.click()
+
+        # 5.1
+
+        assert hold.is_selected()
+
+        # 5.2
+
+        on_hold = (By.XPATH, "//*[contains(@text, ':')]")
+
+        WebDriverWait(driver, 5).until(
+            EC.text_to_be_present_in_element(on_hold, "На удержании")
+        )
+
+        element = driver.find_element(*on_hold)
+        assert element.text == "На удержании"
+
+        # 5.3 cannot check music
+
+        # 5.4
+
+        driver.screenshot("./../screenshots/actual/53609/on_hold.png")
+
+        # step 6
+
+        hold.click()
+
+        # 6.1
+
+        assert not hold.is_selected()
+
+        # 6.2
+
+        assert re.match(r"^\d{2}:\d{2}:\d{2}$", element.text)
+
+        # 6.3 cannot check music
+
+        # 6.4
+
+        driver.screenshot("./../screenshots/actual/53609/back_to_call.png")
+
+        # step 7
 
         # thats a system window and doesn't expose any text, so I cannot find it reliably...
         buttons = driver.find_elements(By.CLASS_NAME, "Button")
         buttons[-1].click()
 
-        # 4.1 call ended
+        # 7.1 call ended
 
         driver.execute_script("app:waitForPageChange", 3000)
 
-        # 4.2
+        # 7.2
 
         history = driver.find_element(By.XPATH, "//*[contains(@text, 'История')]")
         assert history.is_displayed()
 
-        # 4.3
+        # 7.3
 
         window_id = driver.execute_script(
             "appium:findWindowByElement",
@@ -213,40 +268,40 @@ class TestIncomingCall50557:
 
         assert re.match(r"^\d{2}:\d{2}:\d{2}$", time_text)
 
-        # 4.4
+        # 7.4
 
-        driver.screenshot("./../screenshots/actual/50557/call_ended.png")
+        driver.screenshot("./../screenshots/actual/53609/call_ended.png")
 
-        # step 5
+        # step 8
 
         button_close = driver.find_element(By.CLASS_NAME, "Button")
         button_close.click()
 
-        # 5.1
+        # 8.1
 
         elements = driver.find_elements(
             By.XPATH, "//*[contains(@text, 'Вызов завершен')]"
         )
         assert len(elements) == 0
 
-        # 5.2
+        # 8.2
 
-        driver.screenshot("./../screenshots/actual/50557/close_pop_up.png")
+        driver.screenshot("./../screenshots/actual/53609/close_pop_up.png")
 
-        # step 6
+        # step 9
 
         driver.execute_script("app:swipe", "up")
 
-        # 6.1 && 6.2
+        # 9.1 && 9.2
 
-        driver.screenshot("./../screenshots/actual/50557/home.png")
+        driver.screenshot("./../screenshots/actual/53609/home.png")
 
-        # step 7
+        # step 10
 
         widget = driver.find_element(By.XPATH, "//*[contains(@text, 'Call API DBus')]")
         widget.click()
 
-        # 7.1
+        # 10.1
 
         state = driver.query_app_state("ru.auroraos.demos/CallApiDBus")
         assert state == "RUNNING_IN_FOREGROUND"
@@ -258,106 +313,23 @@ class TestIncomingCall50557:
         main_page = driver.find_element(By.ID, "mainPage")
         assert main_page.is_displayed()
 
-        # 7.2
+        # 10.2
 
-        driver.screenshot("./../screenshots/actual/50557/return_to_main.png")
+        hold_second = driver.find_element(By.XPATH, "//*[contains(@text, 'Удержание')]")
+        assert hold_second.is_selected()
 
-        # step 8
+        # 10.3
 
-        incoming.click()
+        driver.screenshot("./../screenshots/actual/53609/return_to_main.png")
 
-        # 8.1
+        # step 11
 
-        left = set()
-        numbers = []
+        hold_second.click()
 
-        start_time = time.time()
+        # 11.1
 
-        assert not incoming.is_selected() and not outgoing.is_selected()
-        assert not hold_switch.is_selected() and not dtmf_switch.is_selected()
+        assert not hold_second.is_selected()
 
-        while time.time() - start_time < 6:
-            text = driver.find_element(By.ID, "incomingCallButton").text
+        # 11.2
 
-            match = re.search(r"\((\d+)\)", text)
-            if match:
-                val = int(match.group(1))
-                left.add(val)
-                numbers.append(val)
-
-            time.sleep(0.2)
-
-        # here I check that the timer was actually updating
-        assert len(left) >= 5
-
-        # 8.2 and 8.3
-
-        # checked in previous step while checking timer
-
-        # 8.4
-
-        driver.screenshot("./../screenshots/actual/50557/timer.png")
-
-        # 8.5
-
-        contact = WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//*[contains(@text, 'Remote name')]")
-            )
-        )
-
-        assert contact.is_displayed()
-
-        # 8.6 I cannot check vibration state here
-
-        # 8.7
-
-        driver.screenshot("./../screenshots/actual/50557/start_incoming.png")
-
-        # step 9
-
-        red = driver.find_element(By.XPATH, "//*[contains(@text, 'Отклонить')]")
-
-        red.click()
-
-        # 9.1
-
-        driver.execute_script("app:waitForPageChange", 3000)
-
-        # 9.2
-
-        window_id = driver.execute_script(
-            "appium:findWindowByElement",
-            {"strategy": By.XPATH, "value": "//*[contains(@text, 'Вызов завершен')]"},
-        )
-
-        driver.execute_script("appium:setWindow", {"windowId": window_id})
-
-        ended = driver.find_element(By.XPATH, "//*[contains(@text, 'Вызов завершен')]")
-        assert ended.is_displayed()
-
-        remote = driver.find_element(By.XPATH, "//*[contains(@text, 'Remote name')]")
-
-        assert remote.is_displayed()
-
-        time_element = driver.find_element(
-            By.XPATH, "//*[contains(@text, ':') and contains(@text, '0')]"
-        )
-
-        time_text = time_element.text
-
-        assert re.match(r"^\d{2}:\d{2}:\d{2}$", time_text)
-
-        assert time_text == "00:00:00"
-
-        # 9.3
-
-        driver.screenshot("./../screenshots/actual/50557/call_ended_right_away.png")
-
-        # step 10
-
-        WebDriverWait(driver, 10).until(
-            EC.invisibility_of_element_located(
-                (By.XPATH, "//*[contains(@text, 'Remote name')]")
-            )
-        )
+        driver.screenshot("./../screenshots/actual/53609/main_hold_off.png")
